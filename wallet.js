be0b2bfbecebe913525d23e515fb266b2d10ebe2
anas20023 <yorder07@gmail.com>
2023-12-03 09:15:03 +0600

    Update wallet.html and wallet.js

--- wallet.js
+++ wallet.js
@@ -1,1 +1,54 @@
-alert("Welcome to Dashboard!");
+var dep_prev = 0,
+  wd_prev = 0,
+  dip,
+  wd,
+  curr_b = 1340;
+
+
+  // For Diposite 
+
+
+function GetValue() {
+  dip = document.getElementById("d_add").value;
+}
+function Click_Dip() {
+  if (dip > 0) {
+    dep_prev += parseInt(dip);
+    curr_b += parseInt(dip);
+    document.getElementById("pre_dip").innerHTML = dep_prev;
+    document.getElementById("cur_b").innerHTML = curr_b;
+  }
+  else
+  {
+    alert("Amount Must Be Possitive!");
+  }
+}
+
+  // For Withdraw
+
+function GetValue_W()
+{
+    wd = document.getElementById("w_add").value;
+}
+
+function Click_Wth()
+{
+    if(wd>0)
+    {
+        if(wd<=curr_b)
+        {
+            wd_prev += parseInt(wd);
+            curr_b -= parseInt(wd);
+            document.getElementById("pre_wd").innerHTML = wd_prev;
+            document.getElementById("cur_b").innerHTML = curr_b;
+        }
+        else
+        {
+            alert("Insufficient Balance!");
+        }
+    }
+    else
+    {
+        alert("Amount Must Be Possitive!");
+    }
+}
